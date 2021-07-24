dcc52066844951b26a7a37ee0656791fd3aff127
elucasticus <79570276+elucasticus@users.noreply.github.com>
2021-07-24 20:46:23 +0200

    Add files via upload

--- forketthubapp.py
+++ forketthubapp.py
@@ -19,13 +19,18 @@ img_anova1 = Image.open("anova1.png")
 img_anova2 = Image.open("anova2.png")
 img_anova3 = Image.open("anova3.png")
 img_anova4 = Image.open("anova4.png")
+img_modellolineare1 = Image.open("modellolineare1.png")
+img_modellolineare2 = Image.open("modellolineare2.png")
+img_modellolineare3 = Image.open("modellolineare3.png")
+img_modellolineare4 = Image.open("modellolineare4.png")
 
 # define functions
 def modellocompleto(distanzaduomo, capitaleiniziale, giorni, livelloprezzo, dipendenti, indipendenti, michelin):
-    return distanzaduomo*(-1.298e+02)  + distanzaduomo * distanzaduomo * 1.439e-02 + capitaleiniziale  *  1.374e+00 + giorni * 3.126e+01 + dipendenti  *  6.744e+04 + livelloprezzo *  8.768e+04 + indipendenti  *   5.547e+04 + dipendenti*michelin * 1.794e+04
+    return distanzaduomo* -1.299e+02  + distanzaduomo*distanzaduomo * 1.439e-02  + capitaleiniziale  *  1.374e+00  + giorni* 3.126e+01  + dipendenti  *  6.745e+04   + livelloprezzo   * 8.758e+04 + indipendenti  *   5.556e+04  + dipendenti*michelin * 1.794e+04
+
 
 def modelloridotto(distanzaduomo, capitaleiniziale, giorni, livelloprezzo, kerneldistanceconst):
-    return distanzaduomo*(-7.766e+01) + distanzaduomo*distanzaduomo*1.030e-02 + capitaleiniziale* 3.214e+00  + giorni* 8.478e+01  + livelloprezzo *2.105e+05 + kerneldistanceconst * 1.568e+04
+    return  distanzaduomo * -3.023e+01+ distanzaduomo*distanzaduomo*1.410e-02+ capitaleiniziale* 3.180e+00 + giorni* 8.038e+01  + livelloprezzo *3.003e+05+ kerneldistanceconst * 9.499e+03  + distanzaduomo*livelloprezzo * -5.319e+01  
 
 def modelloutile(nreviews, giorni):
     return  -40446.012 + 20.533*nreviews + 7.709*giorni
@@ -116,6 +121,19 @@ df.at[1, 'lng'] = 9.1919
 main_menu = st.selectbox("", ["Place your restaurant", "Curiosities", "Competition index"])
 if main_menu == "Place your restaurant":
 
+    st.write('Here you can find and play with two different models to predict the revenue of a restaurant')
+    st.write('The first is a very accurate and interpretable model for the prediction of the income of a restaurant. The best use of this is to predict the revenue of an already existing restaurant since most of the variables are unknown at the opening of the business. It’s very robust with only few outliers in the predictors.')
+    st.image(img_modellolineare1)
+    st.write('The prediction interval will be in the form:')
+    st.image(img_modellolineare2)
+    st.write('We estimated (through LOO Cross Validation) an interval of this form with  α=0.143 would contain the real value 93% of the times and the parameters are estimated through OLS. R^2=0.86')
+    st.write('The second quit robust model is intended to be used to predict the revenue of a restaurant someone intend to open.')
+    st.image(img_modellolineare3)
+    st.write('The prediction interval will be in the form:')
+    st.image(img_modellolineare4)
+    st.write('We estimated (through LOO Cross Validation) an interval of this form with  α=0.431 would contain the real value 89% of the times. R^2=0.56')
+    st.write('Here you can fit the models with your data and find the best position to open your own restaurant')
+
     # submit and geocode location
     location = st.text_input('Insert your location here (default position: "Milan Cathedral, Milan")')
     livelloprezzo = st.slider("Price level", 1, 4)
@@ -129,7 +147,7 @@ if main_menu == "Place your restaurant":
     indipendenti = st.text_input('Number of founders: (only for the complete model)', value = 0)
     indipendenti = int(indipendenti)
     michelin = st.checkbox('Any michelin star? (only for the complete model)')
-
+    
     if st.button("Submit"):
         g = geocoder.osm(location)
         if g.ok:
@@ -142,9 +160,10 @@ if main_menu == "Place your restaurant":
             distanzadalduomo = hs.haversine(currentlocation, duomo)*1000
             fatturatoridotto = modelloridotto(distanzadalduomo, capitaleiniziale, 0., livelloprezzo, kerneldistanceconst)
             fatturatocompleto= modellocompleto(distanzadalduomo, capitaleiniziale, giorni, livelloprezzo, dipendenti, indipendenti, michelin)
-            alpha = 0.1
-            st.success(str(fatturatoridotto*(1 - alpha)) + ', ' + str(fatturatoridotto*(1+alpha)))
-            st.success(str(fatturatocompleto*(1 - alpha)) + ', ' + str(fatturatocompleto*(1+alpha)))
+            alpha = 0.45
+            st.success('Second model: net income ∈ [' + str(int(fatturatoridotto*(1 - alpha))) + ', ' + str(int(fatturatoridotto*(1+alpha))) + ']')
+            alpha = 0.15
+            st.success('First model: net income ∈ [' + str(int(fatturatocompleto*(1 - alpha))) + ', ' + str(int(fatturatocompleto*(1+alpha))) + ']')
         else:
             st.error("Location not found")
 
@@ -171,9 +190,11 @@ elif main_menu == "Curiosities":
         ))
     st.text('Blue dot = no Michelin stars\nRed dot = at least one Michelin star')
     st.write('The ANOVA test Ricavi  ~ Michelin aims at investigating differences of revenues between the Michelin-starred restaurants in Milan and the others. There is statistical evidence of a difference in the mean of the two groups, and the starred restaurants turn out to have higher mean revenue. This can be explained with the fact that such types of restaurants usually make bigger investments, have more workers and have a high level of price of the menu, so there is a large turnover.')
+    st.write('A confidence interval for the difference of the mean of non Michelin and Michelin restaurants is (-894160, -79374.8) at level 95%.')
     st.image(img_anova1)
-    st.write('Things change when we inspect the variable “Utile o perdite” which represents the profit. The ANOVA tests shows that there isn’t a significant difference in the mean of the profit of the two categories of restaurants (starred and not). Like we said, starred restaurants may be big or with a large turnover but in the end, they also have a lot of expenses (skilled chefs, quality of the food…) and the gain is not different from the other structures.')
+    st.write('Things change when we inspect the variable “Utile o perdite” which represents the profit. The ANOVA test shows that there isn’t a significant difference in the mean of the profit of the two categories of restaurants (starred and not). Like we said, starred restaurants may be big or with a large turnover but in the end, they also have a lot of expenses (skilled chefs, quality of the food…) and the gain is not different from the other structures.')
     st.image(img_anova2)
+    st.write('A confidence interval for the difference of the mean of Michelin and non-Michelin restaurants is (-21675.94, 86102.08) at level 95%.')
     
     # show map with different colors for restaurants depending on their distance from the center of the city
     st.pydeck_chart(
@@ -187,6 +208,7 @@ elif main_menu == "Curiosities":
     st.text('Blue dot = centre\nRed dot = suburbs\nGreen dot = middle')
     st.write('We may want to highlight the role of the position over the revenues from a categorical point of view. We have created a variable which values are “suburbs” for the places more than three kilometres distant from the centre, which we identify with the Milan Cathedral, “middle” for restaurants with intermediate distance (between one and three kilometres from the Cathedral) and “centre” for the others. Three kilometres from the cathedral is approximately the radius of the ring road of Milan.  The places situated near the centre have higher mean revenues, since they are the most visited from tourists and office workers and they are usually more expensive, so they have large incomings. The difference in the mean revenues is not very significant when we analyse the groups “suburbs” and “middle”.')
     st.image(img_anova3)
+    st.write('Confidence intervals at level 95% for the difference of the mean revenues are: (122136.03, 572802.7) for the difference between the restaurants in the centre and the restaurants in the middle; (248470.76, 742855.0) for the difference between the restaurants in the centre and the restaurants in the suburbs; (-22120.37, 318507.4) for the difference between the restaurants in the middle and the restaurants in the suburbs')
 
     # show map with different colors for restaurants depending on their rating
     st.pydeck_chart(
@@ -200,6 +222,7 @@ elif main_menu == "Curiosities":
     st.text('Blue dot = 3 stars\nRed dot = 4 stars\nGreen dot = 5 stars')
     st.write('Another interesting ANOVA test is ratings ~ iscenter. It shows us that the mean of the ratings for the restaurants in one zone is not different from the one of restaurants in the others, even though there is a difference in the mean revenues. The rating is not a measure of the size of the business but is related with quality and subjective factors which are similar for the suburbs, for the middle and for the centre and that we cannot inspect with our database.')
     st.image(img_anova4)
+    st.write('Confidence intervals at level 95% for the difference of the mean ratings are: (-0.09716711, 0.04990659) for the difference between the restaurants in the centre and the restaurants in the middle; ( -0.10449932, 0.05684150) for the difference between the restaurants in the centre and the restaurants in the suburbs; (-0.05578008, 0.05538278) for the difference between the restaurants in the middle and the restaurants in the suburbs')
 
 ###---------------------------
 ### Gaussian kernel competition index
